642d75ffe4123caff57b73abecf7b7cbc3f91e46
HK <hitesh@99fusion.com>
2017-01-12 12:24:03 +0530

    class and actions separated

--- admin/wplms_buy_batch_class.php
+++ admin/wplms_buy_batch_class.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Initialise WPLMS Buy Batches
+ *
+ * @class       WPLMS_Buy_Batch_Class
+ * @author      H.K. Latiyan
+ * @category    Admin
+ * @package     WPLMS-Buy-Batch/admin
+ * @version     1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 class WPLMS_Buy_Batch_Class{
 
@@ -11,7 +24,6 @@ class WPLMS_Buy_Batch_Class{
 
 	private function __construct(){
 		add_shortcode('wplms_buy_batch', array($this,'wplms_buy_batch_shortcode'));
-		add_action('wp_ajax_buy_wplms_batch',array($this,'buy_wplms_batch'));
 
 	} // END public function __construct
 
@@ -68,39 +80,4 @@ class WPLMS_Buy_Batch_Class{
 
 	}
 
-	function buy_wplms_batch(){
-
-		if(isset($_POST['batch_name']) && isset($_POST['batch_seats']) && isset($_POST['batch_courses'])){
-
-			$batch_name = $_POST['batch_name'];
-			$courses = $_POST['batch_courses'];
-			$batch_seats = $_POST['batch_seats'];
-			$user_id = get_current_user_id();
-
-			$group_settings = array(
-	            'creator_id' => $user_id,
-	            'name' => $batch_name,
-	            'status' => 'private',
-	            'date_created' => current_time('mysql')
-	        );
-
-	        /* Create group/batch */
-	        $group_id = groups_create_group( $group_settings);
-
-	        if(is_numeric($group_id)){
-
-	        	/* Add batch settings */
-	        	groups_update_groupmeta( $group_id, 'total_member_count', 1 );
-            	groups_update_groupmeta( $group_id, 'last_activity', gmdate( "Y-m-d H:i:s" ) );
-            	groups_update_groupmeta( $group_id, 'course_batch',1);
-            	foreach ($courses as $course_id) {
-            		groups_add_groupmeta($group_id,'batch_course',$course_id);
-            	}
-            	groups_update_groupmeta( $group_id, 'enable_seats', 1 );
-            	groups_update_groupmeta( $group_id, 'batch_seats',$batch_seats);
-            	groups_update_groupmeta( $group_id, 'batch_exclusivity', 1 );
-	        }
-		}
-	}
-
-}
+} // End of class WPLMS_Buy_Batch_Class
