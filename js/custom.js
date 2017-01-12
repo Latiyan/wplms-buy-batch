e13dd37719d222b79b4f6e76dff2b71e23a71564
HK <hitesh@99fusion.com>
2017-01-12 11:46:26 +0530

    Ajax call for buy batch.

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($){
+
+	$('#wplms_buy_batch').on('click',function(){
+		var batch_name = $('.wplms_buy_batch_form').find('.batch_name').val();
+		var batch_courses = $('.wplms_buy_batch_form').find('.batch_courses').val();
+		var batch_seats = $('.wplms_buy_batch_form').find('.batch_seats').val();
+		$.ajax({
+            type: "POST",
+            url: ajaxurl,
+            data: { action: 'buy_wplms_batch',
+            		batch_name: batch_name,
+            		batch_courses: batch_courses,
+            		batch_seats: batch_seats,
+                  },
+            cache: false,
+            success: function (html) {
+
+            }
+        });
+	});
+
+});
